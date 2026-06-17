92d514e933e02b78c0a1a116896221d7566287da
vashisthneelakshi-star <vashisthneelakshi@gmail.com>
2026-06-17 10:56:27 +0530

    Delete app/admin directory

--- app/admin/providers.tsx
+++
@@ -1,5 +0,0 @@
-'use client'
-import { SessionProvider as NextSessionProvider } from 'next-auth/react'
-export function SessionProvider({ children }: { children: React.ReactNode }) {
-  return <NextSessionProvider>{children}</NextSessionProvider>
-}
